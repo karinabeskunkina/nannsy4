ee993e18218fd3797606605adf5c92e2910827be
Karina Lavrenieva <lkvlkvlkv2003@yandex.ru>
2022-03-09 15:40:04 +0300

    Kommid Mass

---
+++ cmake-build-debug/CMakeFiles/nannsy4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nannsy4.dir/main.cpp.obj"
+  "libnannsy4.dll.a"
+  "nannsy4.exe"
+  "nannsy4.exe.manifest"
+  "nannsy4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nannsy4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/nannsy4.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for nannsy4.
+# This may be replaced when dependencies are built.
